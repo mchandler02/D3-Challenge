02b86913d6acbed1fe3bfff8e72acdae12805214
Molly Sullivan <mchandler02@gmail.com>
2021-04-26 20:38:21 -0500

    chart area added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 1000;
+var svgHeight = 500;
+var margin = {top:10, right: 30, bottom: 30, left: 60};
+var width = svgWidth-margin.left-margin.right;
+var height = svgHeight-margin.top-margin.bottom;
+
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
